34e8d4d3eaed6df78e69e486a34c55438d0d68e9
Emmanuel Colas <emmanuel.colas@bioserenity.com>
2021-11-03 13:32:05 +0100

    Rush zombie instead of human

--- codingame-zombies-ia/src/main/java/fr/li212/codingame/zombies/ia/ComputePosition.java
+++ codingame-zombies-ia/src/main/java/fr/li212/codingame/zombies/ia/ComputePosition.java
@@ -3,7 +3,6 @@ package fr.li212.codingame.zombies.ia;
 import fr.li212.codingame.zombies.ia.entities.Coordinate;
 import fr.li212.codingame.zombies.ia.entities.Situation;
 import fr.li212.codingame.zombies.ia.entities.actors.Ash;
-import fr.li212.codingame.zombies.ia.entities.actors.Human;
 import fr.li212.codingame.zombies.ia.entities.actors.Zombie;
 
 import java.util.Comparator;
@@ -12,7 +11,7 @@ public class ComputePosition {
     public Coordinate compute(final Situation situation) {
         final Ash ash = situation.getAsh();
 
-        final Human mostAtRiskHumanThanCanBeSaved = situation.getHumans().stream()
+        final Zombie zombieClosestToMostAtRiskHumanThanCanBeSaved = situation.getHumans().stream()
                 .sorted((human1, human2) -> {
                     final Zombie zombieClosestToHuman1 = situation.getZombies().stream().min(Comparator.comparingInt(zc -> human1.getCoordinate().squareDistance(zc.getCoordinate()))).get();
                     final Zombie zombieClosestToHuman2 = situation.getZombies().stream().min(Comparator.comparingInt(zc -> human2.getCoordinate().squareDistance(zc.getCoordinate()))).get();
@@ -23,8 +22,9 @@ public class ComputePosition {
                     double distanceHumanZombie = Math.sqrt(closestZombie.getCoordinate().squareDistance(human.getCoordinate()));
                     double distanceAshZombieInRange = Math.sqrt(closestZombie.getCoordinate().squareDistance(ash.getCoordinate())) - 2000;
                     return distanceHumanZombie / GlobalParameters.ZOMBIE_SPEED > distanceAshZombieInRange / GlobalParameters.ASH_SPEED;
-                }).findFirst().get();
+                }).map(human -> situation.getZombies().stream().sorted(Comparator.comparingInt(z -> human.getCoordinate().squareDistance(z.getCoordinate()))).findFirst().get()).findFirst().get();
 
-        return mostAtRiskHumanThanCanBeSaved.getCoordinate();
+
+        return zombieClosestToMostAtRiskHumanThanCanBeSaved.getCoordinate();
     }
 }
